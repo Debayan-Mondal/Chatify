af589820e344bd8328563c1cb5c1e868bc53b920
Debayan <debayanwb17@gmail.com>
2026-02-18 15:40:08 +0530

    Adding signup backend

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,8 +2,9 @@ import mongoose, { mongo } from "mongoose"
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Mongo Database Connected...");
+        if (!MONGO_URI) throw new Error("MONGO_URI is not set")
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log("Mongo Database Connected...", conn.connection.host);
     } catch(err) {
         console.error("Error Connection to Mongodb", err);
         process.exit(1)
