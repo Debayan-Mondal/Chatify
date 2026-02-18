35b8a52f118e883ff1e74b8a024e840d969e9d5c
Debayan <debayanwb17@gmail.com>
2026-02-19 00:07:02 +0530

    Implemeneted the login and logout api endpoint

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -61,3 +61,30 @@ export const signup = async (req, res) => {
         res.status(500).json({message: "Internal Server Error"});
     }
 }
+
+export const login = async (req, res) => {
+    try {
+            const {email, password} = req.body;
+            const user = await User.findOne({email});
+
+            if (!user) return res.status(400).json({message: "Invalid Credentials"});
+            const isPasswordMatched = await bcrypt.compare(password, user.password);
+            if(!isPasswordMatched) return res.status(400).json({message: "Invalid Credentials"});
+    
+            generateToken(user._id, res);
+            res.status(200).json({
+                _id:user._id,
+                fullName: user.fullName,
+                email: user.email,
+                profilePic: user.profilePic,
+            });
+        } catch(err) {
+            console.error("Error in login controller...", err);
+            res.status(500).json({message: "Internal Server Error"});
+    }
+}
+
+export const logout = (req, res) => {
+    res.cookie("jwt", "", {maxAge:0});
+    res.status(200).json({message: "Logged out Successfully"});
+}
